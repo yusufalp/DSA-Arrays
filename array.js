433ab5d7d2c0c705059f359f103242a2d8873b8d
Yusuf Alp <alpyusuf007@gmail.com>
2020-09-24 10:34:56 -0700

    Added methods to array

---
+++ array.js
@@ -0,0 +1,130 @@
+const memory = require('./memory');
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = memory.allocate(this.length);
+  }
+
+  push(value) {
+    if (this.length >= this.capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error('Out of memory');
+    }
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+    this._capacity = size;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    return memory.get(this.ptr + index);
+  }
+
+  pop() {
+    if (this.length == 0) {
+      throw new Error('Index error');
+    }
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+    this.length++;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+    this.length--;
+  }
+
+  replace(value, replacement){
+    if (this.length >= this.capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+    memory.get()
+  }
+
+  filter(){
+
+  }
+
+  maxSum(){
+
+  }
+
+  merge(){
+
+  }
+
+  remove(){
+
+  }
+
+  productsOther(){
+
+  }
+
+  zeroColumn(){
+
+  }
+
+  strRotation(){
+    
+  }
+}
+
+Array.SIZE_RATIO = 3;
+
+function main() {
+  let arr = new Array();
+
+  arr.push(3);
+
+  console.log(arr);
+
+  arr.push(5);
+  arr.push(10);
+  arr.push(19);
+
+  console.log(arr);
+
+  arr.pop();
+  arr.pop();
+  arr.pop();
+
+  console.log(arr);
+
+
+  console.log(arr[0]);
+
+  arr.push('tauhida');
+
+  console.log(arr[0]);
+
+
+}
